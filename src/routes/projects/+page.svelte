ee8f3e048c3c1f3de1d01f5daabed0367e41dc2c
trence <tjtong2006@gmail.com>
2023-11-09 19:37:36 -0800

    added some projects

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,46 @@
+<svelte:head>
+	<title>Terence Tong | Projects</title>
+</svelte:head>
+
+<div class="main">
+	<h1>Projects</h1>
+	
+	<div>
+		<h2>Here are a list of projects that I've worked on:</h2>
+		<ul>
+			<li><a href="https://github.com/turrence/spotify-new-music-sorter">spotify life snapshot playlists</a></li>
+			<li><a href="https://github.com/turrence/spotify-music-distributor">spotify playlist distributor</a></li>
+		</ul>
+	</div>
+	<div>
+		<h2>Here are some projects that I've contributed to:</h2>
+		<ul>
+			<li><a href="https://github.com/jaasonw/job-website">job website for new entry level software engineers</a></li>
+			<li><a href="https://github.com/selynna/liquid-hacks">hackathon valorant fantasy league</a></li>
+		</ul>
+	</div>
+	<div>
+		<h1>Project Concepts</h1>
+		<ul>
+			<li><h2>Unbiased Political Information</h2></li>
+			<p>
+				This idea revolves around getting data directly from the <a href="https://gpo.congress.gov/">congress API</a> to report on policy without the bias of any corporation. This 
+				project would make reading the primary source of our congressional hearings as simple as possible. This project involves summarizing
+				information from those hearings and presenting them in an easily digestable format.
+			</p>
+		</ul>
+		<ul>
+			<li><h2>Correlation between Walkability Score and Mesasures of Happiness in the US</h2></li>
+				<a href="https://catalog.data.gov/dataset/walkability-index1">walkability index</a>
+				<p>need link for Measures of Happiness in the US
+		</ul>
+	</div>
+</div>
+
+
+
+<style>
+	.main {
+		padding-top: 50px;
+	}
+</style>
